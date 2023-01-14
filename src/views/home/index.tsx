394d19203591fd0bbb5e468b5aa22a9e1d7a11f9
xiaqingsong <927538064@qq.com>
2023-01-15 05:07:53 +0800

    feat首页文字

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -7,7 +7,8 @@ const Home = memo(() => {
       <div className='home-banner'>
         <div className='banner-text'>
           <span>XQS-Blog</span>
-          <p>只要朝着一个方向努力，一切都会变得得心应手</p>
+          {/* 每一个不曾起舞的日子，都是对生命的辜负 */}
+          <p>且视他人之疑目如盏盏鬼火，大胆地去走你的夜路</p>
           <div><button>Enter Blog</button></div>
         </div>
       </div>
