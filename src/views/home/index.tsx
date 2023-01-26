d06c92b0d33e6f99cd08ed6978d355a857197aa6
xiaqingsong <927538064@qq.com>
2023-01-26 17:04:08 +0800

    feat  17点04分

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,7 +1,10 @@
 import React, { memo } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './index.scss'
 
 const Home = memo(() => {
+  const navigate = useNavigate()
+
   return (
     <div>
       <div className='home-banner'>
@@ -9,7 +12,7 @@ const Home = memo(() => {
           <span>XQS-Blog</span>
           {/* 每一个不曾起舞的日子，都是对生命的辜负 */}
           <p>且视他人之疑目如盏盏鬼火，大胆地去走你的夜路</p>
-          <div><button>Enter Blog</button></div>
+          <div><button onClick={() => navigate('/category')}>Enter Blog</button></div>
         </div>
       </div>
       <div className='content'></div>
