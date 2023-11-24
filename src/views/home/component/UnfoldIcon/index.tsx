75e985798b9c5213949eda85444814be9bda5ec3
xiaqingsong <927538064@qq.com>
2023-11-25 04:01:04 +0800

    test

---
+++ src/views/home/component/UnfoldIcon/index.tsx
@@ -0,0 +1,59 @@
+import {FC, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import classNames from "classnames";
+
+import './index.scss'
+
+interface IUnfoldIcon {}
+
+const UnfoldIcon: FC<IUnfoldIcon> = props => {
+    const navigate = useNavigate()
+    const [isAnimation, setIsAnimation] = useState(false)
+    const [iconClassName, setIconClassName] = useState('header-icon')
+    const [navClassName, setNavClassName] = useState('rNav')
+
+    const handleChangeIcon = () => {
+        setIsAnimation(!isAnimation)
+        if(iconClassName !== 'header-icon icon-out') {
+            setIconClassName('header-icon icon-out');
+        }
+        if(isAnimation) {
+            setNavClassName('rNav')
+        } else {
+            setNavClassName('rNav showNav')
+        }
+    };
+
+    const nav = () => {
+        return [
+            {name: '首页', path: '/'},
+            {name: '音乐', path: '/music'},
+            {name: '分类', path: '/category'},
+            {name: '文件', path: '/file'},
+        ]
+    };
+
+  return (
+      <>
+          <div className={classNames((isAnimation ? 'header-icon1 icon-click' : iconClassName), 'headerIcon')} onClick={handleChangeIcon}>
+              <span></span><span></span><span></span>
+          </div>
+          <div className={navClassName}>
+              <div className='nav'>
+                  <div className={'w-[140px] h-[500px] absolute top-[170px] right-0'}>
+                      {
+                          nav().map(item => {
+                              return (
+                                  <div className={'h-[60px] leading-[60px] text-[18px] text-slate-800 cursor-pointer hover:text-blue-400'}
+                                       onClick={() => navigate(item.path)}>{item.name}</div>
+                              )
+                          })
+                      }
+                  </div>
+              </div>
+          </div>
+      </>
+  )
+};
+
+export default UnfoldIcon;
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,56 +1,17 @@
 import {FunctionComponent, useState} from 'react'
 import {useNavigate} from 'react-router-dom'
+
+import UnfoldIcon from './component/UnfoldIcon'
 import './index.scss'
 
 interface IHomeProps {}
 
 const Home: FunctionComponent<IHomeProps> = props => {
-  // react-router 6 中使用这个hook代替history.push,需要使用replace的话配置{replace:true}
-  const navigate = useNavigate()
-  const [isAnimation, setIsAnimation] = useState(false)
-  const [iconClassName, setIconClassName] = useState('header-icon')
-  const [navClassName, setNavClassName] = useState('rNav')
-
-  const handleChangeIcon = () => {
-    setIsAnimation(!isAnimation)
-    if(iconClassName !== 'header-icon icon-out') {
-      setIconClassName('header-icon icon-out');
-    }
-    if(isAnimation) {
-      setNavClassName('rNav')
-    } else {
-      setNavClassName('rNav showNav')
-    }
-  }
-
-  const nav = () => {
-    return [
-      {name: '首页', path: '/'},
-      {name: '音乐', path: '/music'},
-      {name: '分类', path: '/category'},
-      {name: '文件', path: '/file'},
-    ]
-  }
+  const navigate = useNavigate();
 
   return (
       <div>
-        <div className={(isAnimation ? 'header-icon1 icon-click' : iconClassName)} onClick={handleChangeIcon}>
-          <span></span><span></span><span></span>
-        </div>
-        <div className={navClassName}>
-          <div className='nav'>
-            <div className={'w-[140px] h-[500px] absolute top-[170px] right-0'}>
-              {
-                nav().map(item => {
-                  return (
-                      <div className={'h-[60px] leading-[60px] text-[18px] text-slate-800 cursor-pointer hover:text-blue-400'}
-                           onClick={() => navigate(item.path)}>{item.name}</div>
-                  )
-                })
-              }
-            </div>
-          </div>
-        </div>
+        <UnfoldIcon />
         <div className='home-banner'>
           <div className='banner-text'>
             <span>XQS-Blog</span>
